c7e12092286e5298811395174033dd6fe50d9640
Tarion <Tarion@Crudedragos>
2013-01-21 21:35:56 -0400

    Cathedral Expanded Horizontal walls. Cleaned up front face.

--- src/projectzulu/common/world/structures/cemetary/BlueprintCathedralEntrance.java
+++ src/projectzulu/common/world/structures/cemetary/BlueprintCathedralEntrance.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 
+import projectzulu.common.core.BoundaryPair;
 import projectzulu.common.world.blockdataobjects.BlockWithMeta;
+import projectzulu.common.world.blockdataobjects.Returnable;
 import projectzulu.common.world.cell.CellIndexDirection;
 import projectzulu.common.world.structures.Blueprint;
 import projectzulu.common.world.structures.BlueprintHelper;
@@ -13,44 +15,137 @@ public class BlueprintCathedralEntrance extends Blueprint{
 
 	@Override
 	public BlockWithMeta getBlockFromBlueprint(int cellIndex, int cellSize,
-			int curHeight, int maxHeight, int xIndex, int zIndex,
-			Random random, CellIndexDirection cellIndexDirection) {
+			int curHeight, int maxHeight, int xIndex, int zIndex, Random random, CellIndexDirection cellIndexDirection) {
+		Returnable<BlockWithMeta> returnableValue = new Returnable<BlockWithMeta>();
+		/* Create Ceiling */
 		switch (cellIndexDirection) {
-		case SouthWestCorner:
 		case NorthWestCorner:{
-			BlockWithMeta returnValue = null;
-			int rowIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int colIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int rowIndex = cellIndexDirection.getNorthSouthIndex(cellIndex, cellSize);
+			/* Top Ceiling Roof */
 			if(curHeight > maxHeight - cellSize){
-				returnValue = BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 0), cellSize-rowIndex-1, 1, curHeight, maxHeight-cellSize/3, cellIndex, cellSize, new BlockWithMeta(0));
-			}else if(curHeight > maxHeight - 2*cellSize){
-				returnValue = BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), cellSize-rowIndex-1, 2, curHeight, maxHeight - cellSize, cellIndex, cellSize);	
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 0), cellSize-colIndex-1, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 1, curHeight, maxHeight-cellSize/3, new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 0),
+						new BlockWithMeta(0)));
 			}
-			returnValue = returnValue != null ? returnValue : BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*3/10);
-			if(rowIndex > 1 && returnValue == null){
-				returnValue = BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-2);
+			/* Secondary Ceiling Roof */
+			else if(curHeight > maxHeight - 2*cellSize){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), cellSize-colIndex-1, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 2, curHeight, maxHeight - cellSize, new BlockWithMeta(Block.stoneBrick.blockID, 0)));
 			}
-			if(returnValue != null){ return returnValue; }
+			
+			/* West Wall */
+			if(curHeight+1 < cellSize + curHeight-1 - (cellSize + curHeight-2) % cellSize){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0),
+						curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*3/10));
+			}
+			/* Create Floor, Ground and Balcony*/
+			if(curHeight == 0){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}else if(curHeight+1 == cellSize + curHeight-1 - (cellSize + curHeight-2) % cellSize){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}
 			break;
 		}
-		case SouthEastCorner:
+		case SouthWestCorner:{
+			int colIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int rowIndex = cellIndexDirection.getNorthSouthIndex(cellIndex, cellSize);
+			/* Top Ceiling Roof */
+			if(curHeight > maxHeight - cellSize){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 0), cellSize-colIndex-1, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 1, curHeight, maxHeight-cellSize/3, new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 0),
+						new BlockWithMeta(0)));
+			}
+			/* Secondary Ceiling Roof */
+			else if(curHeight > maxHeight - 2*cellSize && ((colIndex > 1 && rowIndex < cellSize-3) || rowIndex < cellSize-2)){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), cellSize-colIndex-1, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 2, curHeight, maxHeight - cellSize, new BlockWithMeta(Block.stoneBrick.blockID, 0)));
+			}
+			/* West Wall */
+			returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0),
+					curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*3/10));
+			
+			/* Outside 'Front' Wall */
+			if(colIndex > 1){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0),
+						curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-3));
+			}else{
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0),
+						curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-2));
+			}
+			
+			/* Create Floor, Ground and Balcony*/
+			if(curHeight == 0){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}else if(curHeight+1 == cellSize + curHeight-1 - (cellSize + curHeight-2) % cellSize){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}
+			break;
+		}		
+		case SouthEastCorner:{
+			int colIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int rowIndex = cellIndexDirection.getNorthSouthIndex(cellIndex, cellSize);
+			/* Top Ceiling Roof*/
+			if(curHeight > maxHeight - cellSize){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 1), colIndex, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 1, curHeight, maxHeight-cellSize/3, new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 1),
+						new BlockWithMeta(0)));
+			}
+			/* Secondary Ceiling Roof */
+			else if(curHeight > maxHeight - 2*cellSize && ( (colIndex<cellSize-3 && rowIndex < cellSize-3) || rowIndex < cellSize-2)){ //|
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), colIndex, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 2, curHeight, maxHeight - cellSize, new BlockWithMeta(Block.stoneBrick.blockID, 0)));
+			}
+			/* East Wall */
+			returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight,
+					cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*7/10));
+			/* Outside 'Front' Wall */
+			if(colIndex < cellSize - 3){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight,
+						maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-3));
+			}else{
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight,
+						maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-2));
+			}
+			
+			/* Create Floor, Ground and Balcony*/
+			if(curHeight == 0 && ((colIndex<cellSize-3 && rowIndex < cellSize-3) || rowIndex < cellSize-2)){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}else if(curHeight+1 == cellSize + curHeight-1 - (cellSize + curHeight-2) % cellSize 
+					&& ( (colIndex<cellSize-3 && rowIndex < cellSize-3) || rowIndex < cellSize-2)){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}
+			break;
+		}
 		case NorthEastCorner:{
-			BlockWithMeta returnValue = null;
-			int rowIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int colIndex = CellIndexDirection.getWestEastIndex(cellIndex, cellSize);
+			int rowIndex = cellIndexDirection.getNorthSouthIndex(cellIndex, cellSize);
+			/* Top Ceiling Roof*/
 			if(curHeight > maxHeight - cellSize){
-				returnValue = BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 1), rowIndex, 1, curHeight, maxHeight-cellSize/3, cellIndex, cellSize, new BlockWithMeta(0));
-			}else if(curHeight > maxHeight - 2*cellSize){
-				returnValue = BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), rowIndex, 2, curHeight, maxHeight - cellSize, cellIndex, cellSize);
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 1), colIndex, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 1, curHeight, maxHeight-cellSize/3, new BlockWithMeta(Block.stairsStoneBrickSmooth.blockID, 1),
+						new BlockWithMeta(0)));
 			}
-			returnValue = returnValue != null ? returnValue : BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*7/10);
-			if(rowIndex < cellSize - 3 && returnValue == null){
-				returnValue = BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight, cellIndex, cellSize, CellIndexDirection.WestWall, cellSize-2);
+			/* Secondary Ceiling Roof */
+			else if(curHeight > maxHeight - 2*cellSize){
+				returnableValue.acceptIfEmpty(BlueprintHelper.getSlopeBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), colIndex, 
+						BoundaryPair.createPair(0, (cellSize-1)*2), 2, curHeight, maxHeight - cellSize, new BlockWithMeta(Block.stoneBrick.blockID, 0)));
 			}
-			if(returnValue != null){ return returnValue; }
+			/* East Wall */
+			returnableValue.acceptIfEmpty(BlueprintHelper.getSolidWallBlock(new BlockWithMeta(Block.stoneBrick.blockID, 0), curHeight, maxHeight,
+					cellIndex, cellSize, CellIndexDirection.NorthWall, cellSize*7/10));
+			
+			/* Create Floor, Ground and Balcony*/
+			if(curHeight == 0){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}else if(curHeight+1 == cellSize + curHeight-1 - (cellSize + curHeight-2) % cellSize){
+				returnableValue.acceptIfEmpty(new BlockWithMeta(Block.stoneBrick.blockID, 0));
+			}
 			break;
-		}			
+		}		
 		default: break;
-		}
-		return new BlockWithMeta(0);
+		}		
+		return returnableValue.isReturnablePresent() ? returnableValue.getReturnableObject() : new BlockWithMeta(0);
 	}
 	
 	@Override
